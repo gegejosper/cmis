d6935ff7277dfbfcdc9e0d1b36485cbb45c870af
Gegejosper Ceniza <gegejosper@gmail.com>
2024-11-21 10:19:32 +0800

    add favicon

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -44,7 +44,7 @@ class RegisteredUserController extends Controller
         event(new Registered($user));
 
         Auth::login($user);
-
-        return redirect(route('panel/dashboard', absolute: false));
+        return redirect(route('panel.dashboard'));
+        //return redirect(route('panel', absolute: false));
     }
 }
